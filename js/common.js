88750843d45d7fbba5d8f6c97f688eb960f2ac59
repeto1204 <45305363+repeto1204@users.noreply.github.com>
2019-01-29 14:08:34 +0900

    author : JJH | notice.html 반응형 작업 완료

--- js/common.js
+++ js/common.js
@@ -1,11 +1,20 @@
 let main_height = document.querySelector('.main-box').scrollHeight;
 let side_height = document.querySelector('.side-nav');
-console.log(window.innerHeight);
-console.log(main_height);
-if(main_height >=window.innerHeight) {
+
+if(window.innerWidth<=768) {
+    side_height.style.height = 'auto';
+} else if(main_height >=window.innerHeight) {
     side_height.style.height = main_height + 'px';
 } else {
     side_height.style.height= (window.innerHeight-70) + 'px';
-}
+};
 
-console.log(side_height.style.height);
+window.addEventListener("resize", function(){
+    if(window.innerWidth<=768) {
+        side_height.style.height = 'auto';
+    } else if(main_height >=window.innerHeight) {
+        side_height.style.height = main_height + 'px';
+    } else {
+        side_height.style.height= (window.innerHeight-70) + 'px';
+    };
+});
--- js/notice.js
+++ js/notice.js
@@ -10,7 +10,7 @@ console.log(mod_chk.length);
 mod_btn.addEventListener('click', function(){
     prev_chk_ary=[];
     mod_btn.style.display = 'none';
-    confirm_btn.style.display = 'block';
+    confirm_btn.style.display = 'flex';
     for(let i of mod_chk){
         i.style.display = 'block';
         prev_chk_ary.push(i.checked);
